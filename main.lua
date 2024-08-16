9748ee855c7fb52f00bc1b20da838c9ad7ca8e9f
Liana <lianahockin@gmail.com>
2024-08-15 21:41:16 -0400

    Circle Or Rectangle Double Players

--- main.lua
+++ main.lua
@@ -12,4 +12,46 @@ function love.errorhandler(msg)
     end
 end
 
-print("hello World")
+print("play test 01")
+
+function love.load()
+    circlexpos = 390
+    rectanglexpos = 400
+    rectangleypos = 400
+    circleypos = 290
+    moveSpeed = 100
+    move = true
+end
+
+function love.update(deltaTime)
+    --print(deltaTime)
+    --if love.keyboard.isDown("d") then
+    -- circlexpos = circlexpos + moveSpeed * deltaTime
+    -- end
+    -- if love.keyboard.isDown("a") then
+    --   circlexpos = circlexpos - moveSpeed * deltaTime
+    -- end
+    -- if love.keyboard.isDown("s") then
+    --    circleypos = circleypos + moveSpeed * deltaTime
+    -- end
+    -- if love.keyboard.isDown("w") then
+    --     circleypos = circleypos - moveSpeed * deltaTime
+    --  end
+    if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
+        rectanglexpos = rectanglexpos + moveSpeed * deltaTime
+    end
+    if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
+        rectanglexpos = rectanglexpos - moveSpeed * deltaTime
+    end
+    if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
+        rectangleypos = rectangleypos + moveSpeed * deltaTime
+    end
+    if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
+        rectangleypos = rectangleypos - moveSpeed * deltaTime
+    end
+end
+
+function love.draw()
+    love.graphics.rectangle("line", rectanglexpos, rectangleypos, 200, 150)
+    --love.graphics.circle("line", circlexpos, circleypos, 100)
+end
